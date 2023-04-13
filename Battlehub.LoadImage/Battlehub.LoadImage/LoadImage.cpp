625c041cd374ae1c449cec5d53d34fd1b81a0853
Vadym <Vadim.Andriyanov@outlook.com>
2023-04-13 21:47:47 +0300

    load image from memory

--- Battlehub.LoadImage/Battlehub.LoadImage/LoadImage.cpp
+++ Battlehub.LoadImage/Battlehub.LoadImage/LoadImage.cpp
@@ -7,6 +7,40 @@
 
 #include "LoadImage.h"
 
+int CalculateMipmapCount(int width, int height)
+{
+	int maxDimension = fmax(width, height);
+	int mipLevels = (int)log2(maxDimension) + 1;
+	return mipLevels;
+}
+
+int CalculateMipmapArraySize(int width, int height, int channels, int mipmapCount)
+{
+	int totalSize = 0;
+	int currentWidth = width;
+	int currentHeight = height;
+
+	for (int i = 0; i < mipmapCount; i++)
+	{
+		totalSize += currentWidth * currentHeight * channels;
+		currentWidth = fmax(1, currentWidth / 2);
+		currentHeight = fmax(1, currentHeight / 2);
+	}
+
+	return totalSize;
+}
+
+void GenerateMipmaps(unsigned char* bytes, int width, int height, int channels, int mipmapCount)
+{
+	for (int i = 0; i < mipmapCount - 1; ++i)
+	{
+		stbir_resize_uint8(bytes, width, height, 0, bytes + width * height * channels, width / 2, height / 2, 0, channels);
+		bytes += width * height * channels;
+		width /= 2;
+		height /= 2;
+	}
+}
+
 ImageInfo Battlehub_LoadImage_GetInfo(const Char* file)
 {
 #if _WIN32
@@ -20,6 +54,7 @@ ImageInfo Battlehub_LoadImage_GetInfo(const Char* file)
 	return info;
 }
 
+
 void Battlehub_LoadImage_Load(const Char* file, void* outData, int channels, int mipmapCount)
 {
 #if _WIN32
@@ -32,21 +67,60 @@ void Battlehub_LoadImage_Load(const Char* file, void* outData, int channels, int
 	stbi_set_flip_vertically_on_load(1);
 
 	int width, height, n;
-	unsigned char* pixeldata = stbi_load(path, &width, &height, &n, channels);
-	if (pixeldata == nullptr)
+	stbi_uc* data = stbi_load(path, &width, &height, &n, channels);
+	if (data == NULL)
 	{
 		return;
 	}
 
-	unsigned char* ptr = (unsigned char*)outData;
-	memcpy(ptr, pixeldata, width * height * channels);
-	stbi_image_free(pixeldata);
-
-	for (int i = 0; i < mipmapCount - 1; ++i)
-	{
-		stbir_resize_uint8(ptr, width, height, 0, ptr + width * height * channels, width / 2, height / 2, 0, channels);
-		ptr += width * height * channels;
-		width /= 2;
-		height /= 2;
-	}
+	stbi_uc* ptr = (stbi_uc*)outData;
+	memcpy(ptr, data, width * height * channels);
+	stbi_image_free(data);
+	GenerateMipmaps(ptr, width, height, channels, mipmapCount);
 }
+
+ImageInfo Battlehub_LoadImage_Load_From_Memory(void* inData, int size, bool mipChain, void** outData)
+{
+	stbi_set_flip_vertically_on_load(1);
+
+	ImageInfo info = { 0 };
+
+	stbi_uc* ptr = (stbi_uc*)inData;
+	stbi_uc* data = stbi_load_from_memory(ptr, size, &info.width, &info.height, &info.channels, 0);
+	if (data == NULL)
+	{
+		return info;
+	}
+
+	int mipmapCount = 1;
+	if (mipChain)
+	{
+		mipmapCount = CalculateMipmapCount(info.width, info.height);
+	}
+
+	size = CalculateMipmapArraySize(info.width, info.height, info.channels, mipmapCount);
+	ptr = (stbi_uc*)malloc(size);
+	if (ptr == NULL)
+	{
+		stbi_image_free(data);
+		return info;
+	}
+
+	memcpy(ptr, data, info.width * info.height * info.channels);
+	stbi_image_free(data);
+
+	if (mipChain)
+	{
+		GenerateMipmaps(ptr, info.width, info.height, info.channels, mipmapCount);
+	}
+
+	*outData = ptr;
+	info.status = 1;
+	return info;
+}
+
+void Battlehub_LoadImage_Free_Memory(void* data)
+{
+	free(data);
+}
+
--- Battlehub.LoadImage/Battlehub.LoadImage/LoadImage.h
+++ Battlehub.LoadImage/Battlehub.LoadImage/LoadImage.h
@@ -20,5 +20,9 @@ extern "C"
 {
     DLLExport ImageInfo Battlehub_LoadImage_GetInfo(const Char* path);
 
-    DLLExport void Battlehub_LoadImage_Load(const Char* path, void* bytes, int channels, int mipLevels);
+    DLLExport void Battlehub_LoadImage_Load(const Char* path, void* bytes, int channels, int mipmapCount);
+
+    DLLExport ImageInfo Battlehub_LoadImage_Load_From_Memory(void* bytes, int size, bool mipChain, void** outBytes);
+
+    DLLExport void Battlehub_LoadImage_Free_Memory(void* data);
 }
